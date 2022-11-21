68ad2f89fb9d194e536faf1e2dc549aac4130d39
stan-nsa <stan.nsa@gmail.com>
2022-11-22 00:00:41 +1000

    q

--- hdd-nsa-bot.py
+++ hdd-nsa-bot.py
@@ -1,32 +1,22 @@
-import telebot
-import random
-from telebot import types
+from aiogram import Bot, Dispatcher, executor, types
 
-# Создаем бота
-bot = telebot.TeleBot('5661825852:AAFU7t-PSe5smkgDKnSYORKewHfMUoD6bL8')
+API_TOKEN = '5661825852:AAFU7t-PSe5smkgDKnSYORKewHfMUoD6bL8'
 
-# Команда start
-@bot.message_handler(commands=["start"])
-def start(m, res=False):
-        # Добавляем две кнопки
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("Факт")
-        item2=types.KeyboardButton("Поговорка")
-        markup.add(item1)
-        markup.add(item2)
-        bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-# Получение сообщений от юзера
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    # Если юзер прислал 1, выдаем ему случайный факт
-    if message.text.strip() == 'Факт' :
-            answer = 'Fuckt'
-    # Если юзер прислал 2, выдаем умную мысль
-    elif message.text.strip() == 'Поговорка':
-            answer = 'Pogovorka'
-    # Отсылаем юзеру сообщение в его чат
-    bot.send_message(message.chat.id, answer)
 
-# Запускаем бота
-bot.polling(none_stop=True, interval=1)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\nЯ Эхо-бот!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+#test2
+
