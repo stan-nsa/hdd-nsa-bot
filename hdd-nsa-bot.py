dcf9c5bf3eae84b9f50b135694a878c461e3ef20
stan-nsa <stan.nsa@gmail.com>
2022-11-22 23:38:07 +1000

    q

--- hdd-nsa-bot.py
+++ hdd-nsa-bot.py
@@ -13,6 +13,19 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.Message):
     await message.reply("Привет!\nЯ Эхо-бот!\nОтправь мне любое сообщение, а я тебе обязательно отвечу!")
 
+@dp.message_handler(commands=['schedule'])
+async def send_schedule_team(message: types.Message):
+    await message.reply("Расписание матчей <team>:\n. . .\n. . .\n. . .\n. . .\n. . .\n")
+
+@dp.message_handler(commands=['today'])
+async def send_schedule_today(message: types.Message):
+    from datetime import date
+    await message.reply("Расписание матчей на сегодня - "+date.today().strftime("%d %B %Y")+":\n. . .\n. . .\n. . .\n. . .\n. . .\n")
+
+@dp.message_handler(commands=['results'])
+async def send_results_today(message: types.Message):
+    from datetime import date
+    await message.reply("Результаты матчей на сегодня - "+date.today().strftime("%d %B %Y")+":\n. . .\n. . .\n. . .\n. . .\n. . .\n")
 
 @dp.message_handler()
 async def echo(message: types.Message):
