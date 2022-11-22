1f6024b51dd4cb5f0db8b4a62d66efb81ecc1b9d
stan-nsa <stan.nsa@gmail.com>
2022-11-23 01:24:03 +1000

    nsa

--- hdd-nsa-bot.py
+++ hdd-nsa-bot.py
@@ -35,3 +35,7 @@ async def echo(message: types.Message):
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
 
+# Schedule:  https://statsapi.web.nhl.com/api/v1/schedule?expand=schedule.teams,schedule.scoringplays,schedule.game.seriesSummary,seriesSummary.series,schedule.linescore
+#            https://statsapi.web.nhl.com/api/v1/schedule?date=2022-11-23
+# Standings: https://statsapi.web.nhl.com/api/v1/standings
+# Boxscore:  https://statsapi.web.nhl.com/api/v1/game/2021021092/boxscore
