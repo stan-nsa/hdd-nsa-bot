414ca6da843d545d64858b3887722a5d237e55c4
stan-nsa <stan.nsa@gmail.com>
2022-11-23 10:44:32 +1000

    nsa

---
+++ nhl.py
@@ -0,0 +1,5 @@
+# Schedule:  https://statsapi.web.nhl.com/api/v1/schedule?expand=schedule.teams,schedule.scoringplays,schedule.game.seriesSummary,seriesSummary.series,schedule.linescore
+#            https://statsapi.web.nhl.com/api/v1/schedule?date=2022-11-23
+# Standings: https://statsapi.web.nhl.com/api/v1/standings
+# Boxscore:  https://statsapi.web.nhl.com/api/v1/game/2021021092/boxscore
+
